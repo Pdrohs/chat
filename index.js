4db281ca86f242e146b8ed0bde2c5eb3825bb674
Pdrohs <pedro@accessfast.com.br>
2017-09-15 16:16:04 -0300

    Commit Inicial

---
+++ index.js
@@ -0,0 +1,23 @@
+/// <reference path="typings/node/node.d.ts"/>
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var express = require('express');
+
+app.use('/scripts', express.static(__dirname + '/node_modules/jScrollPane/'));
+app.use('/images', express.static(__dirname + '/images/'));
+
+app.get('/', function(req, res){
+  	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	
+  socket.on('message', function(msg, nome, hora){
+    io.emit('message', msg, nome, hora);
+  });
+});
+
+http.listen(3000, function(){
+	console.log('Servidor ouvindo na porta 3000');
+});
